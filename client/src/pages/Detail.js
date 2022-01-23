198ced7f55d3d740505cdc680843fa0363a28e43
Caeldeth <sabrael@hybrasyl.com>
2022-01-23 01:10:12 -0600

    module 3

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -6,6 +6,7 @@ import Cart from "../components/Cart";
 import { useStoreContext } from "../utils/GlobalState";
 import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY, ADD_TO_CART, UPDATE_PRODUCTS } from "../utils/actions";
 import { QUERY_PRODUCTS } from "../utils/queries";
+import { idbPromise } from "../utils/helpers";
 import spinner from "../assets/spinner.gif";
 
 function Detail() {
@@ -19,15 +20,31 @@ function Detail() {
     const { products, cart } = state;
 
     useEffect(() => {
+        // already in global store
         if (products.length) {
             setCurrentProduct(products.find((product) => product._id === id));
-        } else if (data) {
+        }
+        // retrieved from server
+        else if (data) {
             dispatch({
                 type: UPDATE_PRODUCTS,
                 products: data.products,
             });
+
+            data.products.forEach((product) => {
+                idbPromise("products", "put", product);
+            });
         }
-    }, [products, data, dispatch, id]);
+        // get cache from idb
+        else if (!loading) {
+            idbPromise("products", "get").then((indexedProducts) => {
+                dispatch({
+                    type: UPDATE_PRODUCTS,
+                    products: indexedProducts,
+                });
+            });
+        }
+    }, [products, data, loading, dispatch, id]);
 
     const addToCart = () => {
         const itemInCart = cart.find((cartItem) => cartItem._id === id);
@@ -37,11 +54,16 @@ function Detail() {
                 _id: id,
                 purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1,
             });
+            idbPromise("cart", "put", {
+                ...itemInCart,
+                purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1,
+            });
         } else {
             dispatch({
                 type: ADD_TO_CART,
                 product: { ...currentProduct, purchaseQuantity: 1 },
             });
+            idbPromise("cart", "put", { ...currentProduct, purchaseQuantity: 1 });
         }
     };
 
@@ -50,6 +72,8 @@ function Detail() {
             type: REMOVE_FROM_CART,
             _id: currentProduct._id,
         });
+
+        idbPromise("cart", "delete", { ...currentProduct });
     };
 
     return (
